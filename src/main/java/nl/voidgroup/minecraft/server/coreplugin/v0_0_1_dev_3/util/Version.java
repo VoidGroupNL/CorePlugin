21a4e3b2c6bdf018a581848dca3fe23838dfe14f
VoidStoneDev <janpieter.stolk@kpnmail.nl>
2021-01-19 08:26:14 +0100

    Update Version.java

--- src/main/java/nl/voidgroup/minecraft/server/coreplugin/v0_0_1_dev_3/util/Version.java
+++ src/main/java/nl/voidgroup/minecraft/server/coreplugin/v0_0_1_dev_3/util/Version.java
@@ -1,5 +1,18 @@
 package nl.voidgroup.minecraft.server.coreplugin.v0_0_1_dev_3.versions;
 
 public abstract class Version {
+  private final String[] M_Version;
+  
+  public Version(String[] M_Version) {
+    this.M_Version = M_Version;
+  }
+  
+  public Version setVersionArray(String[] M_Version) {
+    this.M_Version = M_Version;
+  }
+  
+  public String[] getVersionArray() {
+    return 
+  }
 
 }
