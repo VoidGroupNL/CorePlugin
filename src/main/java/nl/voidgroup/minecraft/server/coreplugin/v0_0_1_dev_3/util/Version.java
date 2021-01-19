227144f5cc50cdd352a33bbbb996b3a251c6e78c
Cooleman2007 <67163402+Cooleman2007@users.noreply.github.com>
2021-01-19 11:23:39 +0100

    Updated Version.validVersion regex

--- src/main/java/nl/voidgroup/minecraft/server/coreplugin/v0_0_1_dev_3/util/Version.java
+++ src/main/java/nl/voidgroup/minecraft/server/coreplugin/v0_0_1_dev_3/util/Version.java
@@ -3,12 +3,12 @@ package nl.voidgroup.minecraft.server.coreplugin.v0_0_1_dev_3.util;
 import java.util.regex.Pattern;
 
 public abstract class Version {
-  private static String validVersion = "/[^0-9.]/";
+  private static String validVersion = "/[^0-9.]|^\\.|\\.$|\\.\\./g";
   private final String M_Version;
   
   public Version(String M_MainVersion, int M_VersionPriority, int M_SubVersion) {
     if(Pattern.matches(validVersion, M_MainVersion)) {
-      throw new IllegalArgumentException("M_MainVersion contains illegal characters");
+      throw new IllegalArgumentException("M_MainVersion is invalid");
     }
     M_Version = M_MainVersion + '.' + M_VersionPriority + '.' + M_SubVersion;
   }
